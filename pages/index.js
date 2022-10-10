4d508c127d46640ddb6397d15f3d421b368185ff
unknown <bieel180360@gmail.com>
2022-10-10 01:37:39 -0300

    projeto alisson

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import react from "react";
+import React from "react";
+
+const users = [];
 
 class LoginForm extends React.Component {
     constructor(props) {
@@ -20,8 +23,15 @@ class LoginForm extends React.Component {
     }
 
     handleSubmit(event) {
-        alert("Logado com sucesso: " + this.state.user);
+        const userExists = users.some((user) => {
+            return (
+                user.userName === this.state.user &&
+                user.password === this.state.password
+            );
+        });
+
         event.preventDefault();
+        userExists ? this.props.showPageMethod() : alert("Usuário não existe!");
     }
 
     render() {
@@ -60,7 +70,7 @@ class LoginForm extends React.Component {
 class SignUpForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { name: "", user: "", password: "" };
+        this.state = { name: "", user: "", password: "", confirmPassword: "" };
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -77,7 +87,22 @@ class SignUpForm extends React.Component {
     }
 
     handleSubmit(event) {
-        alert("Cadastrado com sucesso: " + this.state.name);
+        var userNameExists = users.some((user) => {
+            return user.userName === this.state.user;
+        });
+
+        if (
+            !userNameExists &&
+            this.state.password === this.state.confirmPassword
+        ) {
+            alert(`Cadastrado com sucesso ${this.state.user}!`);
+            users.push({
+                userName: this.state.user,
+                password: this.state.password,
+            });
+        }
+
+        console.log(users);
         event.preventDefault();
     }
 
@@ -121,9 +146,9 @@ class SignUpForm extends React.Component {
                 <label>
                     Confirmar senha:
                     <input
-                        name="password"
+                        name="confirmPassword"
                         type="password"
-                        value={this.state.password}
+                        value={this.state.confirmPassword}
                         onChange={this.handleInputChange}
                     />
                 </label>
@@ -142,38 +167,96 @@ class FormControl extends React.Component {
     }
 
     showLogin() {
-        this.setState((state) => ({
+        this.setState(() => ({
             showlogin: true,
             showsignup: false,
         }));
     }
 
     showSignUp() {
-        this.setState((state) => ({
+        this.setState(() => ({
             showlogin: false,
             showsignup: true,
         }));
     }
 
     render() {
+        if (!this.props.show) {
+            return null;
+        }
+
         return (
             <div>
                 <button onClick={this.showLogin}>Login</button>
                 <button onClick={this.showSignUp}>Sign Up</button>
-                <LoginForm show={this.state.showlogin} />
+
+                <LoginForm
+                    show={this.state.showlogin}
+                    showPageMethod={this.props.showPageMethod}
+                />
                 <SignUpForm show={this.state.showsignup} />
             </div>
         );
     }
 }
 
-function Home() {
+function Page(props) {
+    if (!props.show) {
+        return null;
+    }
+
     return (
         <div>
-            <h1>Página inicial do LUIZAO</h1>
-            <FormControl />
+            <h1>Você está logado!</h1>
+            <h2>Faça logout para retornar ao form.</h2>
+
+            <button onClick={props.showFormMethod}>Logout</button>
         </div>
     );
+}
+
+class InterfaceControl extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { showForm: true, showPage: false };
+        this.showForm = this.showForm.bind(this);
+        this.showPage = this.showPage.bind(this);
+    }
+
+    showForm() {
+        this.setState(() => ({
+            showForm: true,
+            showPage: false,
+        }));
+    }
+
+    showPage() {
+        this.setState(() => ({
+            showForm: false,
+            showPage: true,
+        }));
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Página inicial do LUIZAO</h1>
+
+                <FormControl
+                    show={this.state.showForm}
+                    showPageMethod={this.showPage}
+                />
+                <Page
+                    show={this.state.showPage}
+                    showFormMethod={this.showForm}
+                />
+            </div>
+        );
+    }
+}
+
+function Home() {
+    return <InterfaceControl />;
 }
 
 export default Home;
